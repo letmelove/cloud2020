ecdf5326e8365e1934f146a37ca47448d357c7f7
tx <txipromise@163.com>
2020-12-22 11:21:36 +0800

    将消费者模块注册到eureka中

--- cloud-consumer-order8002/src/main/java/com/order/OrderApp.java
+++ cloud-consumer-order8002/src/main/java/com/order/OrderApp.java
@@ -2,9 +2,12 @@ package com.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderApp {
     public static void main(String[] args) {
         SpringApplication.run(OrderApp.class);
