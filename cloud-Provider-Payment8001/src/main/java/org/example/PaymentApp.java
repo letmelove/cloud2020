263cab6231798d1926c49c9e6ce243ab1ddd8def
tx <txipromise@163.com>
2020-12-22 11:21:10 +0800

    将支付模块注册到eureka中

--- cloud-Provider-Payment8001/src/main/java/org/example/PaymentApp.java
+++ cloud-Provider-Payment8001/src/main/java/org/example/PaymentApp.java
@@ -2,8 +2,10 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentApp {
     public static void main(String[] args) {
         SpringApplication.run(PaymentApp.class);
