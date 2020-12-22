e46188980b74bcd655ae4decd7cd484683597b20
tx <txipromise@163.com>
2020-12-22 14:45:05 +0800

    配置eureka集群

--- cloud-eureka-server7003/src/main/java/com/tx/EurekaServerApp03.java
+++ cloud-eureka-server7003/src/main/java/com/tx/EurekaServerApp03.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class App {
+public class EurekaServerApp03 {
     public static void main(String[] args) {
-        SpringApplication.run(App.class);
+        SpringApplication.run(EurekaServerApp03.class);
     }
 }
