448f0665a0c37d76f868ec6ce940421aff9c3c32
tx <txipromise@163.com>
2021-01-24 17:34:14 +0800

    服务发现：获取eureka上注册的服务的信息

--- cloud-consumer-order8002/src/main/java/com/order/controller/OrderController.java
+++ cloud-consumer-order8002/src/main/java/com/order/controller/OrderController.java
@@ -3,13 +3,20 @@ package com.order.controller;
 
 import com.tx.pojo.CommonResult;
 import com.tx.pojo.Payment;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
+@EnableDiscoveryClient
 public class OrderController {
     //单机版时使用的地址
     //public static final String PAYMENT_URL = "http://localhost:8001";
@@ -18,6 +25,10 @@ public class OrderController {
     public static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
     @Resource
     private RestTemplate restTemplate;
+
+    /*服务发现*/
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @GetMapping("/consumer/payment/create")
     public CommonResult<Payment> create(Payment payment) {
@@ -29,4 +40,25 @@ public class OrderController {
     public CommonResult<Payment> getPayment(@PathVariable("id") long id){
         return restTemplate.getForObject(PAYMENT_URL+"/get/"+id,CommonResult.class);
     }
+
+    /**
+     * 服务发现
+     */
+    @GetMapping("/service")
+    public  void discovery(){
+        List<String> serversList =discoveryClient.getServices();
+        List<String> serviceName=new ArrayList<>();
+        List<ServiceInstance> instanceList;
+        for (String server:serversList){
+            //得到eureka中注册的所有服务名
+            serviceName.add(server);
+        }
+        for (int i = 0; i < serviceName.size(); i++) {
+            instanceList=discoveryClient.getInstances(serviceName.get(i));
+            for (ServiceInstance instance :instanceList) {
+                System.out.println("服务名为："+instance.getServiceId()+"\t主机ip："+instance.getHost()+"\t端口号："+instance.getPort()+"\t地址：："+instance.getUri());
+            }
+        }
+    }
+
 }
